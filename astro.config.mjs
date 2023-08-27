4979756cf6ee5cccc96c1873bcfebb673f1b304f
Doruk Gezici <doruk@gezici.me>
2023-08-27 12:56:32 +0200

    Image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yesilmavihayat.com",
   integrations: [tailwind(), sitemap()],
   experimental: { assets: true },
+  image: { service: sharpImageService() },
   output: "server",
   adapter: process.env.VERCEL ? vercel() : netlify(),
 });
