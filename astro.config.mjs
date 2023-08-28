4ea7b145d8e1f061b83ff46291cc59b33c07460c
Doruk Gezici <doruk@gezici.me>
2023-08-28 18:24:29 +0200

    Package renamed to astro-decap-cms-oauth

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import decapCmsGithub from "astro-decap-cms-github";
+import decapCmsOauth from "astro-decap-cms-oauth";
 import { defineConfig, sharpImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yesilmavihayat.com",
-  integrations: [tailwind(), sitemap(), decapCmsGithub()],
+  integrations: [tailwind(), sitemap(), decapCmsOauth()],
   experimental: {
     assets: true,
   },
