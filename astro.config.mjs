37d72831762f60adebea072313c4d4202b887fd6
Doruk Gezici <doruk@gezici.me>
2023-08-28 15:42:02 +0200

    using astro-decap-cms-github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@ import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import decapCmsGithub from "astro-decap-cms-github";
 import { defineConfig, sharpImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yesilmavihayat.com",
-  integrations: [tailwind(), sitemap()],
-  experimental: { assets: true },
-  image: { service: sharpImageService() },
+  integrations: [tailwind(), sitemap(), decapCmsGithub()],
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: sharpImageService(),
+  },
   output: "server",
   adapter: process.env.VERCEL ? vercel() : netlify(),
 });
