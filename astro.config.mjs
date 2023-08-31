ce74066f236c0159362212370007ad680df1d0de
Doruk Gezici <doruk@gezici.me>
2023-08-31 03:03:26 +0200

    Astro upgraded, switched to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import decapCmsOauth from "astro-decap-cms-oauth";
-import { defineConfig, sharpImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yesilmavihayat.com",
   integrations: [tailwind(), sitemap(), decapCmsOauth()],
-  experimental: {
-    assets: true,
-  },
-  image: {
-    service: sharpImageService(),
-  },
   output: "server",
   adapter: vercel(),
 });
