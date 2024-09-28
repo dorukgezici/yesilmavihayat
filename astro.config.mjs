dfc3c788efa663c7dfd33de61f199551e9420aa5
Doruk Gezici <doruk@gezici.me>
2024-09-28 14:06:54 +0200

    Bumped dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import decapCmsOauth from "astro-decap-cms-oauth";
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://yesilmavihayat.com",
   integrations: [tailwind(), sitemap(), decapCmsOauth()],
   output: "hybrid",
-  adapter: vercel({ webAnalytics: { enabled: true }, speedInsights: { enabled: true } }),
-  image: { service: squooshImageService() },
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    speedInsights: { enabled: true },
+  }),
 });
